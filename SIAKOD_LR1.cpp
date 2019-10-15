aaa1c5eefdcc1e7f6d01721aaa10407019d0161e
Oleg <oleg.fisenko2013@yandex.ru>
2019-10-15 21:30:37 +0300

    Function Void BinIn

--- SIAKOD_LR1.cpp
+++ SIAKOD_LR1.cpp
@@ -26,3 +26,23 @@ putchar(byte & 0x80 ? '1' : '0');
 printf("\n");
 
 }
+
+void BinIn(unsigned char* Buf, int len) {
+
+int i, j;
+
+char ch;
+
+for (i = len - 1; i >= 0; i--)
+
+for (j = 1, Buf[i] = 0; j <= 8; Buf[i] = (Buf[i] << 1) + (ch - '0'), j++) {
+
+while ((ch = getchar()) != '0' && ch != '1');
+
+putchar(ch);
+
+}
+
+printf("\n");
+
+}
