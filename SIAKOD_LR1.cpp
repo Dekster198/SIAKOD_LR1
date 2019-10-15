5b979712305794e3a16471e57d8f0d51cef83e5d
Oleg <oleg.fisenko2013@yandex.ru>
2019-10-15 21:27:51 +0300

    Function Void BinOut

--- SIAKOD_LR1.cpp
+++ SIAKOD_LR1.cpp
@@ -10,3 +10,19 @@ unsigned int a;
 long double b;
 
 };
+
+void BinOut(unsigned char* Buf, int len) {
+
+int i, j;
+
+unsigned char byte;
+
+for (i = len - 1; i >= 0; i--)
+
+for (j = 1, byte = Buf[i]; j <= 8; byte <<= 1, j++)
+
+putchar(byte & 0x80 ? '1' : '0');
+
+printf("\n");
+
+}
